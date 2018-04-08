d48c94ac7ad734096c4f41515b4710363b39aa11
bachirbiaich <bachir.biaich@gmail.com>
2018-04-09 00:53:24 +0200

    add logout from API when 500

--- src/app/Classes/Jwt/jwt.interceptor.ts
+++ src/app/Classes/Jwt/jwt.interceptor.ts
@@ -22,7 +22,7 @@ export class JwtInterceptor implements HttpInterceptor {
       }
     }, (err: any) => {
       if (err instanceof HttpErrorResponse) {
-        if (err.status === 401 || err.status === 0) { 
+        if (err.status === 401 || err.status === 0 || err.status === 500) { 
           SessionService.endSession();
           this.router.navigate(['/login']);
         }
